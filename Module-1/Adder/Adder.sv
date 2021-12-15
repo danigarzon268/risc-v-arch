0aebc1172f42ad0e9d8840547550544f77274769
danigarzon268 <danigarzon.268@gmail.com>
2021-12-15 13:49:07 -0500

    🛠🛠 FIX : Parametrizacion del sumador

--- Module-1/Adder/Adder.sv
+++ Module-1/Adder/Adder.sv
@@ -1,11 +1,10 @@
 module ADDER(
-  input logic [31:0] A,
-  input logic [31:0] B,
+  input logic [31:0] PC,
   output logic [31:0] Adder_Result
 );
   
   always@(*) begin
-  	Adder_Result = A + B;
+  	Adder_Result = 4 + PC;
   end
   
 endmodule

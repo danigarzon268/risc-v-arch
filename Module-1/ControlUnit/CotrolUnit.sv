d49c33f2aa88a9311a424bd25c7e4bc985e2c252
danigarzon268 <danigarzon.268@gmail.com>
2021-12-15 13:56:19 -0500

    🛠 FIX: Se corrigen errores de implementacion

--- Module-1/ControlUnit/CotrolUnit.sv
+++ Module-1/ControlUnit/CotrolUnit.sv
@@ -1,9 +1,21 @@
 module ControlUnit(
+
+    // Entradas
     input [6:0] funct7,
     input [2:0] funct3,
   	input [6:0] Op,
-  	output reg [3:0] aluOp,
-    output reg RUWr
+    // Salidas
+  	output logic [3:0] ALUOp,
+    output logic RUWr,
+  	output logic [4:0] BrOp,
+  	output logic [3:0] ALUASrc,
+    output logic ALUBSrc,
+    output logic [2:0] ImmSrc,
+    output logic DMWr,
+    output logic [2:0] DMCtrl,
+    output logic [1:0] RUDataWrSrc
+
+
 );
   always @(funct3 or funct7 or Op)
     begin
